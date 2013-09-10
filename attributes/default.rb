7e9c1f40b530b29d152be3edb6f595cd5fc970ef
Steve Marshall <steve@nascentguruism.com>
2013-09-10 09:52:37 +0100

    Make release, remote, and architecture configurable.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,10 @@
 default['apt_mirror']['data_dir'] = 'fort'
 default['apt_mirror']['data_dir'] = '/data/apt'
 
+default['apt_mirror']['remote_apt'] = 'http://archive.ubuntu.com/ubuntu'
+default['apt_mirror']['release'] = 'raring'
+default['apt_mirror']['architecture'] = 'amd64'
+
 default['apt_mirror']['apache']['listen_hostname'] = '*'
 default['apt_mirror']['apache']['listen_port'] = '80'
 
