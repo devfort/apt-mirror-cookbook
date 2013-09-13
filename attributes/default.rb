666dee3f91c41fda1538333de0667310fd988003
Steve Marshall <steve@nascentguruism.com>
2013-09-13 21:20:01 +0100

    Set user, dumbo.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['apt_mirror']['data_dir'] = 'fort'
+default['apt_mirror']['user'] = 'fort'
 default['apt_mirror']['data_dir'] = '/data/apt'
 
 default['apt_mirror']['remote_apt'] = 'http://archive.ubuntu.com/ubuntu'
