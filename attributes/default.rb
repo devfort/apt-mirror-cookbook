212c51a9c8ec8b0b0ae6d85976da9c3aa1bd0325
James Aylett <james@tartarus.org>
2015-02-22 10:46:49 +0000

    Move to Ubuntu 14.04 LTS (trusty)

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['apt_mirror']['user'] = 'fort'
 default['apt_mirror']['data_dir'] = '/data/apt'
 
 default['apt_mirror']['remote'] = 'http://archive.ubuntu.com/ubuntu'
-default['apt_mirror']['release'] = 'raring'
+default['apt_mirror']['release'] = 'trusty'
 default['apt_mirror']['architecture'] = 'amd64'
 
 default['apt_mirror']['apache']['listen_hostname'] = 'apt.fort'
